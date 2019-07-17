c6845d036977324843314bf5acd1511a7f04686c
donstany <donstany79@gmail.com>
2019-07-17 10:48:42 +0300

    Added skeleton of project and domain classes

---
+++ SamuraiApp.Domain/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace SamuraiApp.Domain
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Samurai Samurai { get; set; } // Navigation property.  Many Quotes have one Samurai
+        public int SamuraiId { get; set; } // FK to table Samurai, Id column
+    }
+}
---
+++ SamuraiApp.Domain/Samurai.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SamuraiApp.Domain
+{
+    public class Samurai
+    {
+        public Samurai()
+        {
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; } // navigation property One to many relation. One Samurai have many quotes
+        public int BattleId { get; set; } // FK to table Battle, Id column
+    }
+}
