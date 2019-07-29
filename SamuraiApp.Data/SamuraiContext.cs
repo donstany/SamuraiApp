6c8f5955ce2011b02aa6ea00098f9fbb444b1025
donstany <donstany79@gmail.com>
2019-07-29 11:46:02 +0300

    Adding Logging in SamuraiContext

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -8,13 +8,22 @@ namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public static readonly LoggerFactory MyConsoleLoggerFactory = new LoggerFactory(
+            new[] {
+                new ConsoleLoggerProvider((category, level) => category == DbLoggerCategory.Database.Command.Name
+                  && level == LogLevel.Information, true)
+            }
+            );
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = SamuraiAppData; Trusted_Connection = True;"); // Added data provider
+
+            optionsBuilder
+                  .UseLoggerFactory(MyConsoleLoggerFactory)
+                  .UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = SamuraiAppData; Trusted_Connection = True;"); // Added data provider
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
